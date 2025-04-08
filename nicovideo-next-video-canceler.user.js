6da4a6a4699b2fcc24066a0151cc8505bbfe3c64
DNEK <dnek.app@gmail.com>
2025-04-08 16:36:49 +0900

    match all url under https://www.nicovideo.jp/

--- nicovideo-next-video-canceler.user.js
+++ nicovideo-next-video-canceler.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name        nicovideo-next-video-canceler
 // @namespace   https://github.com/dnek
-// @version     1.8
+// @version     1.9
 // @author      dnek
 // @description ニコニコ動画の連続再生オフ機能を、公式より少し便利にします（プレイリストは普通に連続再生、動画終了時に全画面を自動解除）。※公式のプレイヤー設定の「次の動画を自動再生」はONのままにしてください。「nicovideo-autoplay-canceler」「nicovideo-player-expander」は別のスクリプトです。
 // @description:ja    ニコニコ動画の連続再生オフ機能を、公式より少し便利にします（プレイリストは普通に連続再生、動画終了時に全画面を自動解除）。※公式のプレイヤー設定の「次の動画を自動再生」はONのままにしてください。「nicovideo-autoplay-canceler」「nicovideo-player-expander」は別のスクリプトです。
 // @homepageURL https://github.com/dnek/nicovideo-next-video-canceler
 // @updateURL   https://github.com/dnek/nicovideo-next-video-canceler/raw/main/nicovideo-next-video-canceler.user.js
 // @downloadURL https://github.com/dnek/nicovideo-next-video-canceler/raw/main/nicovideo-next-video-canceler.user.js
-// @match       https://www.nicovideo.jp/watch/*
+// @match       https://www.nicovideo.jp/*
 // @grant       none
 // @license     MIT license
 // ==/UserScript==
